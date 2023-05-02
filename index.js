ef4b0e39a5040e995cb7f29ae6147a5d7775286a
W3ap0n-X <andy@rippinghouserecords.com>
2023-05-02 15:05:17 -0400

    Removed ready and interactionCreate events from index.js and placed them in their own individual files to clean up the main file.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // Required packages
 const fs = require('node:fs');
 const path = require('node:path');
+const mongoose = require('mongoose');
 const { Client, Events, Collection, GatewayIntentBits } = require('discord.js');
 
-const { dbConfig } = require('eloquify');
+// const { dbConfig } = require('eloquify');
 require('dotenv').config();
-
+db_connection();
 // Create Discord Bot Connection
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMembers,
+] });
 
 // Bot commands are collected and stored.
 
@@ -25,95 +29,80 @@ for (const file of commandFiles) {
 		client.commands.set(command.data.name, command);
 	}
 	else {
-		console.log(`[WARNING] THe command at ${filePath} is missing a required "data" or "execute" property.`);
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 	}
 }
 
-// Handle Commands
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
 	}
-
-	try {
-		await command.execute(interaction);
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
-	catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-		else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-	console.log(interaction);
-});
-
-// Bot logs into server
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+}
 
 client.login(process.env.BOT_TOKEN);
 
 
-dbConfig({
-	driver: 'my-sql',
-	user: 'aoe2bot',
-	host: 'localhost',
-	database: process.env.DB_NAME,
-	password: process.env.DB_PASS,
-});
+// dbConfig({
+// 	driver: 'my-sql',
+// 	user: 'aoe2bot',
+// 	host: 'localhost',
+// 	database: process.env.DB_NAME,
+// 	password: process.env.DB_PASS,
+// });
 
-const { Model } = require('eloquify');
+// const { Model } = require('eloquify');
 
-class DumbTitle extends Model {
-	init() {
-		this.setTable('dumb_title');
-	}
+// class DumbTitle extends Model {
+// 	init() {
+// 		this.setTable('dumb_title');
+// 	}
+// }
+
+// module.export = DumbTitle;
+
+// async function getTitles() {
+// 	const all_titles = await DumbTitle.all();
+// 	console.log(all_titles);
+// }
+
+// async function getTitleById(title_id = 1) {
+// 	const title = await DumbTitle.find(title_id);
+// 	if (title.stored()) {
+// 		return title;
+// 	}
+// 	else {
+// 		return 'unable to retrieve title';
+// 	}
+// }
+
+// async function updateTitle(title_id, new_title = false, new_description = false, new_points = false) {
+// 	const update_data = {};
+// 	if (new_title != false) {
+// 		update_data.title = new_title;
+// 	}
+// 	if (new_description != false) {
+// 		update_data.description = new_description;
+// 	}
+// 	if (new_points != false) {
+// 		update_data.points = new_points;
+// 	}
+// 	await DumbTitle.update(update_data, title_id);
+// }
+
+// async function addTitle(title, description, points = 1) {
+// 	const new_title = await DumbTitle.create({ title:title, description:description, points:points });
+// 	console.log(new_title);
+// }
+
+async function db_connection() {
+	await mongoose.connect('mongodb://aoe2_ctw_test:' + process.env.DB_PASS + '@127.0.0.1:27017/aoe2_ctw_test');
 }
-
-module.export = DumbTitle;
-
-async function getTitles() {
-	const all_titles = await DumbTitle.all();
-	console.log(all_titles);
-}
-
-async function getTitleById(title_id = 1) {
-	const title = await DumbTitle.find(title_id);
-	if (title.stored()) {
-		return title;
-	}
-	else {
-		return 'unable to retrieve title';
-	}
-}
-
-async function updateTitle(title_id, new_title = false, new_description = false, new_points = false) {
-	const update_data = {};
-	if (new_title != false) {
-		update_data.title = new_title;
-	}
-	if (new_description != false) {
-		update_data.description = new_description;
-	}
-	if (new_points != false) {
-		update_data.points = new_points;
-	}
-	await DumbTitle.update(update_data, title_id);
-}
-
-async function addTitle(title, description, points = 1) {
-	const new_title = await DumbTitle.create({ title:title, description:description, points:points });
-	console.log(new_title);
-}
-
-module.exports.getTitleById = getTitleById;
